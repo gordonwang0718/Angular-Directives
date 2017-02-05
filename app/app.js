f1df9a56b6c956c5ce555f9f0825d655e1fc3c87
gordonwang0718 <Gordon@XuyangdeMacBook-Pro.local>
2017-02-04 22:13:04 -0800

    /*First Directive*/ Using Built-in Directive

--- app/app.js
+++ app/app.js
@@ -8,12 +8,13 @@ angular.module('app').controller('mainCtrl', function($scope) {
 			street: '909 W Temple St',
 			city: 'Los Angeles',
 			nation: 'US'
-		}
+		},
+		color: ['black', 'white', 'gray']
 	}
 });
 angular.module('app').directive('userInfoCard', function(){
 	return {
-		template: "User Info Card: {{ user.name }} <br /> address: {{ user.address.street }} <br /> city: {{ user.address.city }} <br /> contry: {{ user.address.nation}} <br />"
+		template: "User Info Card: {{ user.name }} <br /> <br/> <div ng-show='!!user.address'> Address: {{ user.address.street }} <br /> City: {{ user.address.city }} <br /> Contry: {{ user.address.nation}} <br /></div> <br /> <div> Color: <div ng-repeat='element in user.color'> {{ element }} </div> </div>"
 		// restrict: "C";
 
 	}
