660c3711bfaedfb7cfd95abf698b796a76f975a3
gordonwang0718 <Gordon@XuyangdeMacBook-Pro.local>
2017-02-04 22:02:11 -0800

    /*First Directive*/ Adding Bindings

--- app/app.js
+++ app/app.js
@@ -2,14 +2,19 @@
 
 angular.module('app',[]);
 angular.module('app').controller('mainCtrl', function($scope) {
-
+	$scope.user = {
+		name: 'Gordon Wang',
+		address: {
+			street: '909 W Temple St',
+			city: 'Los Angeles',
+			nation: 'US'
+		}
+	}
 });
 angular.module('app').directive('userInfoCard', function(){
 	return {
-		template:"User Info Card - Gordon"
+		template: "User Info Card: {{ user.name }} <br /> address: {{ user.address.street }} <br /> city: {{ user.address.city }} <br /> contry: {{ user.address.nation}} <br />"
+		// restrict: "C";
 
-		restrict:"E"
-		// AngularJS has a default restriction for directives
-		// "A"-attribute "E"-element "C"-class "M"-comment
 	}
 })
