23b9f27e16dc8cd096b4fd0565918466992fbfa0
gordonwang0718 <Gordon@XuyangdeMacBook-Pro.local>
2017-02-05 00:37:05 -0800

    /*Events & Scopr & Controllers*/ Inherited Scopes

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 
 angular.module('app',[]);
 angular.module('app').controller('mainCtrl', function($scope) {
-	$scope.user = {
+	$scope.user1 = {
 		name: 'Gordon Wang',
 		address: {
 			street: '909 W Temple St',
@@ -11,18 +11,49 @@ angular.module('app').controller('mainCtrl', function($scope) {
 		},
 		color: ['black', 'white', 'gray']
 	}
+	$scope.user2 = {
+		name: 'Jenni Wang',
+		address: {
+			street: '909 W Temple St',
+			city: 'Los Angeles',
+			nation: 'China'
+		},
+		color: ['black', 'white', 'gray']
+	}
 
- 
+
 
 });
 angular.module('app').directive('userInfoCard', function(){
 	return {
 		templateUrl: "file:///Interview/Front-end_Practice/7.%20Angular%20Directives%20Fundamentals/2.%20Events%20&%20Scopr%20&%20Controllers/app/userInfoCard.html",
 		// restrict: "E";
+		scope: {
+			user: '='
+		},
 		controller: function($scope){
-			$scope.clickMe = function(ele){
+			$scope.collapsed = false;
+			$scope.clickMe = function(ele) {
 				ele.show = "Yes Clicked";
 			}
+			$scope.collapse = function() {
+				$scope.collapsed = !$scope.collapsed;
+			}
 		}
+	}
+});
+angular.module('app').directive('addressInfo', function() {
+	return {
+		templateUrl: 'file:///Interview/Front-end_Practice/7.%20Angular%20Directives%20Fundamentals/2.%20Events%20&%20Scopr%20&%20Controllers/app/addressInfo.html',
+		scope: true, // seperate scopr from derective 'userInfoCard'
+		controller: function($scope) {
+      $scope.collapsed = false;
+      $scope.collapseAddress = function() {
+        $scope.collapsed = true;
+      }
+      $scope.expandAddress = function() {
+        $scope.collapsed = false;
+			}
+    }
 	}
 })
